87a01f45c3fe139888a04a0be9cd5a46e04ef139
ChalaniWeerarthna <35931060+ChalaniWeerarthna@users.noreply.github.com>
2021-01-05 23:06:48 +0530

    ColorPicker

---
+++ colorPicker.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+frameWidth = 640;
+frameHieght = 480;
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHieght)
+cap.set(10, 150)  # brightness
+
+
+def empty(a):
+    pass
+
+
+cv2.namedWindow("HSV")
+cv2.resizeWindow("HSV", 640, 240)
+cv2.createTrackbar("Hue Min", "HSV", 0, 179, empty)
+cv2.createTrackbar("Hue Max", "HSV", 179, 179, empty)
+cv2.createTrackbar("Sat Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("Sat Max", "HSV", 255, 255, empty)
+cv2.createTrackbar("Val Min", "HSV", 0, 255, empty)
+cv2.createTrackbar("Val Max", "HSV", 255, 255, empty)
+
+while True:
+    success, img= cap.read()
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "HSV")
+    h_max = cv2.getTrackbarPos("Hue Max", "HSV")
+    s_min = cv2.getTrackbarPos("Sat Min", "HSV")
+    s_max = cv2.getTrackbarPos("Sat Max", "HSV")
+    v_min = cv2.getTrackbarPos("Val Min", "HSV")
+    v_max = cv2.getTrackbarPos("Val Max", "HSV")
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask=mask)
+
+    cv2.imshow("Original", img)
+    cv2.imshow("imageHSV", imgHSV)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Result", imgResult)
+    cv2.waitKey(1)
+
